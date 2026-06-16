1ee170d469b951c3b4143620a693e1c1ae6bd7b5
Radja Mahesa W <radjamahesaw@gmail.com>
2026-06-17 03:27:57 +0700

    Fix password salt type

--- src/lib/password.ts
+++ src/lib/password.ts
@@ -4,7 +4,7 @@ const iterations = 120000;
 const keyLength = 32;
 const digest = "sha256";
 
-export async function hashPassword(password: string, salt = randomUUID()) {
+export async function hashPassword(password: string, salt: string = randomUUID()) {
   const hash = pbkdf2Sync(password, salt, iterations, keyLength, digest);
 
   return `pbkdf2:${iterations}:${salt}:${hash.toString("base64url")}`;
